bff9fdb0eab78d78b4b2a143fc6479740c649181
Atharvagarud137 <92423386+Atharvagarud137@users.noreply.github.com>
2022-05-05 12:24:02 +0530

    Create prims.c

---
+++ prims.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void prims(int, int [10][10]);
+
+void main()
+{
+  int n, cost[10][10];
+  printf("Enter number of vertices:\n");
+  scanf("%d",&n);
+  printf("Enter the cost matrix:\n");
+  for(int i=0; i<n; i++)
+  {
+    for(int j=0; j<n; j++)
+    {
+      scanf("%d", &cost[i][j]);
+    }
+  }
+  for(int i=0; i<n; i++)
+  {
+    for(int j=0; j<n; j++)
+    {
+      if(cost[i][j]==0)
+      {
+        cost[i][j]=999;
+      }
+    }
+  } 
+  prims(n, cost);
+}
+
+void prims(int n, int cost[10][10])
+{
+  int min_cost=0,min,e=1,u=1,v=1,visited[10];
+  for(int i=0; i<=n; i++)
+  {
+    visited[i]=0;
+  }
+  visited[1]=1;
+  while(e<n)
+  {
+    for(int i=0; i<=n; i++)
+    {
+      for(int j=0; j<=n; j++)
+      {
+         if(cost[i][j]<min)
+         {
+           if(visited[i]==0)
+              continue;
+           else
+           {
+             min=cost[i][j];
+             u=i;
+             v=j;
+           }
+         }   
+      }
+      if(visited[u]==0 || visited[v]==0)
+      {
+        min_cost=min_cost+min;
+        printf("%d edge (%d, %d) and cost = %d",++e,u,v,min);
+        visited[v]=1;
+      }
+      cost[u][v]=cost[v][u]=999;
+    }
+    printf("Cost of the MST = %d\n", min_cost);
+  }
+}      
